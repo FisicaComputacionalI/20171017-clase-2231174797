7ba51e98d278365166e18984527baae89d2457f8
2231174797 <31254610+2231174797@users.noreply.github.com>
2017-10-17 13:47:52 -0500

    Add files via upload
    suma = 110

--- suma1.cc
+++ suma1.cc
@@ -1,16 +1,17 @@
 #include <iostream>
 using namespace std;
+
 int main()
 {
   int sum=0;
   int count=1;
-  while(count<10){
-    if((count %2)>0)
+  while(count<21){
+    if((count%2)<=0)
       {
 	sum=sum+count;
       }
     count++;
   }
-  cout<< "suma" << sum << endl;
+  cout<<"suma"<<sum<<endl;
   return 0;
 }
